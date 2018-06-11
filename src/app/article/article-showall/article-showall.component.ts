be33e632190699941a91a3790887cd0171b4c181
Kristi <krsy3ii@yahoo.com>
2018-06-10 19:24:23 -0700

    make changes on article-showall component to compile successfully

--- src/app/article/article-showall/article-showall.component.ts
+++ src/app/article/article-showall/article-showall.component.ts
@@ -13,38 +13,35 @@ export class ArticleShowallComponent implements OnInit {
   currentView = this.allArticles;
   filterByView: string = "allArticles"; //default view
 
-articleSector(clicked){
-  if (clicked ===1){
-    return "POLITICS";
-  } else if (clicked ===2){
-    return "BUSINESS";
-  } else if (clicked ===3){
-    return "MARKETS";
-  } else if (clicked ===4){
-    return "PURSUITS";
-  } else if (clicked ===5){
-    return "ECONOMICS";
-  } else if (clicked ===6){
-    return "OPINION";
-  } else {
-    return "TECHNOLOGY";
-  }
-}
+  constructor(
+    public boolean = false;
+  ) { } //borrows from the Article constructor in Models
 
-toggleSelected(clickedArticle: Article, setSelected: boolean) {
-    clickedArticle.pure = setSelected;
-  }
-//method for filter change
-onChange(optionSelected){
-  this.filterByView = optionSelected;
-}
-
-  constructor() {
-    public Boolean = false;
-    //borrows from the Article constructor in Models
+  articleSector(clicked){
+    if (clicked ===1){
+      return "POLITICS";
+    } else if (clicked ===2){
+      return "BUSINESS";
+    } else if (clicked ===3){
+      return "MARKETS";
+    } else if (clicked ===4){
+      return "PURSUITS";
+    } else if (clicked ===5){
+      return "ECONOMICS";
+    } else if (clicked ===6){
+      return "OPINION";
+    } else {
+      return "TECHNOLOGY";
     }
+  }
 
+  toggleSelected(clickedArticle: Article, setSelected: boolean) {
+    clickedArticle.pure = setSelected;
+    }
+  //method for filter change
+  onChange(optionSelected){
+    this.filterByView = optionSelected;
+  }
   ngOnInit() {
   }
-
 }

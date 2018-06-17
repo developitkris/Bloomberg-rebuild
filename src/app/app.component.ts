2a4cd685e082f4f2e789bdcc49bac512f3df452c
Kristi <krsy3ii@yahoo.com>
2018-06-17 16:57:47 -0700

    move topNews and recentNews into new sample-articles file, set them to empty string[] in their originated components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,6 @@ export class AppComponent {
   // titles: Articles[] = [];
   // authors: Articles[] = [];
   // sectors: Articles[] = [];
-
-  topNews: Article[]= [
-    new Article('Trump Advisers Slam Trudeaus Betrayal Days Before Kim Summit', 'Politics', 'Mark Niquette', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ilHpV14unnK4/v0/800x600.jpg', 'https://www.bloomberg.com/news/articles/2018-06-10/kudlow-blames-trudeau-for-betrayal-before-north-korea-talks', 1),
-    new Article('G-7 Officials Toil Through Night to Paper Over Differences', 'Politics', 'Jennifer Epstein', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iAbOO3CZqDHY/v2/-1x-1.jpg', 'https://www.bloomberg.com/news/articles/2018-06-09/g-7-officials-toil-through-night-to-paper-over-differences', 2),
-    new Article('Asia Stocks See Muted Start to Week; Dollar Drifts: Markets Wrap', 'Markets', 'Cormac Mullen', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iw4tW5PUtL6I/v0/270x203.jpg', 'https://www.bloomberg.com/news/articles/2018-06-10/asia-stocks-see-muted-start-to-week-dollar-drifts-markets-wrap', 3),
-    new Article('Mueller Bolsters Manafort Case With Charges of Suspected Spy', 'Politics', 'Andrew M. Harris', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iulbYPy2oQ.0/v1/-1x-1.jpg', 'https://www.bloomberg.com/news/articles/2018-06-08/manafort-s-ukraine-fixer-indicted-by-u-s-special-counsel', 4),
-    new Article('Brace for the World Economy’s Most Important Week of the Year', 'Business', 'Simon Kennedy', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ieOE9HIamkOk/v0/620x-1.jpg', 'https://www.bloomberg.com/news/articles/2018-06-10/brace-for-the-world-economy-s-most-important-week-of-the-year', 5)
-  ];
+  topNews: Article[];
   currentTitle: string = '';
 }
---
+++ src/app/sample-articles.ts
@@ -0,0 +1,17 @@
+import { Article } from './models/article.model';
+
+export const recentArticles: Article[] = [
+new Article("Merkel Fate Hangs in Balance With Asylum Policy Deadline", "Economics", "Brian Parkin", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iHbcunkLaHaM/v1/1000x-1.jpg", "https://www.bloomberg.com/news/articles/2018-06-17/merkel-fate-hangs-in-balance-as-csu-is-said-to-hand-her-deadline", 1),
+new Article("Democrats Run to the Border to Decry Trump's Splitting of Families", "Politics", "Jennifer Epstein", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i1Nqbub5D6Jw/v0/800x-1.jpg", "https://www.bloomberg.com/news/articles/2018-06-17/trump-s-family-separations-draw-democrats-to-mexican-border", 2),
+new Article("Iran Says Three OPEC Members to Veto Saudi-Proposed Supply Boost", "Business", "Grant Smith", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/irkUMuicnBlI/v0/200x-1.jpg", "https://www.bloomberg.com/news/articles/2018-06-17/iran-says-three-opec-members-to-veto-saudi-proposed-supply-boost", 3),
+new Article("Bitcoin Could Break the Internet, Central Banks' Overseer Says", "Technology", "Edward Robinson", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iqMH0jTEbh1U/v1/800x-1.jpg", "https://www.bloomberg.com/news/articles/2018-06-17/bitcoin-could-break-the-internet-central-banks-overseer-says", 4),
+new Article("Quiet Surface Masks Hints of Fervor in Stocks", "Markets", "Sarah Ponczek", "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ijmT5GGVBEVA/v1/1000x-1.jpg", "https://www.bloomberg.com/news/articles/2018-06-15/speculative-fervor-builds-at-fringe-of-a-quiet-week-for-s-p-500", 5)
+]
+
+export const topNews: Article[] = [
+  new Article('Trump Advisers Slam Trudeaus Betrayal Days Before Kim Summit', 'Politics', 'Mark Niquette', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ilHpV14unnK4/v0/800x600.jpg', 'https://www.bloomberg.com/news/articles/2018-06-10/kudlow-blames-trudeau-for-betrayal-before-north-korea-talks', 1),
+  new Article('G-7 Officials Toil Through Night to Paper Over Differences', 'Politics', 'Jennifer Epstein', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iAbOO3CZqDHY/v2/-1x-1.jpg', 'https://www.bloomberg.com/news/articles/2018-06-09/g-7-officials-toil-through-night-to-paper-over-differences', 2),
+  new Article('Asia Stocks See Muted Start to Week; Dollar Drifts: Markets Wrap', 'Markets', 'Cormac Mullen', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iw4tW5PUtL6I/v0/270x203.jpg', 'https://www.bloomberg.com/news/articles/2018-06-10/asia-stocks-see-muted-start-to-week-dollar-drifts-markets-wrap', 3),
+  new Article('Mueller Bolsters Manafort Case With Charges of Suspected Spy', 'Politics', 'Andrew M. Harris', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iulbYPy2oQ.0/v1/-1x-1.jpg', 'https://www.bloomberg.com/news/articles/2018-06-08/manafort-s-ukraine-fixer-indicted-by-u-s-special-counsel', 4),
+  new Article('Brace for the World Economy’s Most Important Week of the Year', 'Business', 'Simon Kennedy', 'https://assets.bwbx.io/images/users/iqjWHBFdfxIU/ieOE9HIamkOk/v0/620x-1.jpg', 'https://www.bloomberg.com/news/articles/2018-06-10/brace-for-the-world-economy-s-most-important-week-of-the-year', 5)
+]

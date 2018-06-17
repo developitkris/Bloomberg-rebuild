922dd16f6907b145b4874a19105f73bcb7133b09
Kristi <Guest@epicodus-191.local>
2018-06-17 14:12:33 -0700

    add route to home page, showing all articles

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { FeedbackComponent }   from './feedback/feedback.component';
+import { ArticleShowallComponent }   from './article/article-showall/article-showall.component';
 
 const appRoutes: Routes = [
   {
@@ -11,7 +12,11 @@ const appRoutes: Routes = [
   {
    path: 'feedback',
    component: FeedbackComponent
-  }
+ },
+ {
+    path: 'article-showall',
+    component: ArticleShowallComponent
+ }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
